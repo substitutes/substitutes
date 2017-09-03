cd112e2b86cee4a437fd8f3b2e5e0703b4241996
Fronbasal <danielmalik@outlook.de>
2017-09-03 18:58:57 +0200

    Klassen route

--- server.go
+++ server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gin-gonic/gin"
@@ -12,32 +14,104 @@ type class struct {
 	Title string
 }
 
+type vertretung struct {
+	Class   string
+	Std     string
+	Teacher string
+	Subject string
+	Room    string
+	Type    string
+}
+
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		resp, err := request("Druck_Kla.htm")
-		if err != nil {
-			c.JSON(500, gin.H{"message": "Failed to make request", "error": err.Error()})
-		}
-		defer resp.Body.Close()
-		doc, err := goquery.NewDocumentFromReader(resp.Body)
-		if err != nil {
-			c.JSON(500, gin.H{"message": "Failed to read document", "error": err.Error()})
-		}
-		var classes []class
-		doc.Find("table").Last().Find("td").Each(func(i int, sel *goquery.Selection) {
-			href, _ := sel.Find("a").First().Attr("href")
-			title := sel.Text()
-			if title != "" {
-				if title == "---" {
-					title = "Entfall"
-				}
-				classes = append(classes, class{Link: href, Title: title})
+	api := r.Group("api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			resp, err := request("Druck_Kla.htm")
+			if err != nil {
+				c.JSON(500, gin.H{"message": "Failed to make request", "error": err.Error()})
+				return
 			}
+			defer resp.Body.Close()
+			doc, err := goquery.NewDocumentFromReader(resp.Body)
+			if err != nil {
+				c.JSON(500, gin.H{"message": "Failed to read document", "error": err.Error()})
+				return
+			}
+			var classes []class
+			doc.Find("table").Last().Find("td").Each(func(i int, sel *goquery.Selection) {
+				href, _ := sel.Find("a").First().Attr("href")
+				title := sel.Text()
+				if title != "" {
+					if title == "---" {
+						title = "Entfall"
+					}
+					classes = append(classes, class{Link: href, Title: title})
+				}
+			})
+			c.JSON(200, classes)
 		})
-		c.JSON(200, classes)
-	})
+
+		api.GET("/:klasse", func(c *gin.Context) {
+			k := c.Param("klasse")
+			if k == "entfall" {
+				k = "___"
+			} else if !regexp.MustCompile(`^[A-Za-z0-9]+$`).MatchString(k) {
+				c.JSON(400, gin.H{"message": "Invalid class!"})
+				return
+			}
+			resp, err := request("Druck_Kla_" + k + ".htm")
+			if resp.StatusCode != 200 {
+				c.JSON(500, gin.H{"message": "Expected 200, got: " + resp.Status})
+				return
+			}
+			if err != nil {
+				c.JSON(500, gin.H{"message": "Failed to make request", "error": err.Error()})
+				return
+			}
+			defer resp.Body.Close()
+			doc, err := goquery.NewDocumentFromReader(resp.Body)
+			if err != nil {
+				c.JSON(500, gin.H{"message": "Failed to read document", "error": err.Error()})
+				return
+			}
+			var vertretungen []vertretung
+			doc.Find("table").Last().Remove()
+			doc.Find("table").Last().Find("tr").Each(func(i int, sel *goquery.Selection) {
+				if i != 0 {
+					var v vertretung
+					sel.Find("td font").Each(func(i int, sel *goquery.Selection) {
+						t := strings.Replace(sel.Text(), "\n", "", -1)
+						switch i {
+						// dis ugly bc fuck html
+						case 0:
+							v.Class = sel.Find("b").Text()
+							break
+						case 1:
+							v.Std = t
+							break
+						case 2:
+							v.Teacher = t
+							break
+						case 3:
+							v.Subject = t
+							break
+						case 4:
+							v.Room = t
+							break
+						case 5:
+							v.Type = t
+							break
+						}
+					})
+					vertretungen = append(vertretungen, v)
+				}
+			})
+			c.JSON(200, vertretungen)
+		})
+	}
 
 	r.Run(":5000")
 }
