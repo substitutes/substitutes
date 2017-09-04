93d75de2379c56fe38d09ef1b68c81e8b25eec2c
Fronbasal <danielmalik@outlook.de>
2017-09-04 14:32:39 +0200

    Added hires URL and stuff

--- a/index.js
+++ a/index.js
@@ -1,28 +1,28 @@
 $(() => {
-    const klassen = () => {
-        const _x = {
-            a: [9, 8, 7, 6, 5],
-            b: ["a", "b", "c", "d"],
-            c: ["12", "11", "10"]
-        };
-        $.each(_x.a, (a, b) => {
-            $.each(_x.b, (x, y) => {
-                _x.c.push(b + y);
-            });
-        });
-        return _x.c;
+  const klassen = () => {
+    const _x = {
+      a: [9, 8, 7, 6, 5],
+      b: ["a", "b", "c", "d"],
+      c: ["12", "11", "10"]
     };
-    $('select').material_select();
-    $.getJSON("/api", data => {
-        $.each(data, (a, b) => {
-            $("#morgen").append("<option value='" + b + "'>" + b + "</option>");
-        });
-        $.each(klassen(), (a, b) => {
-            $("#alle").append("<option value='" + b + "'>" + b + "</option>");
-        });
-        $('select').material_select();
-        $("select").change(function () {
-            window.location.pathname = "/k/" + $(this).val();
-        });
-    }).catch(e => console.log(e));
+    $.each(_x.a, (a, b) => {
+      $.each(_x.b, (x, y) => {
+        _x.c.push(b + y);
+      });
+    });
+    return _x.c;
+  };
+  $('select').material_select();
+  $.getJSON("/api", data => {
+    $.each(data, (a, b) => {
+      $("#morgen").append("<option value='" + b + "'>" + b + "</option>");
+    });
+    $.each(klassen(), (a, b) => {
+      $("#alle").append("<option value='" + b + "'>" + b + "</option>");
+    });
+    $("select").material_select();
+    $("select").change(function () {
+      window.location.pathname = "/k/" + $(this).val();
+    });
+  }).catch(e => console.log(e));
 });
