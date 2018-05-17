4308dbf3ffdb2b5b6c52ddd407e1e3ed4f1c1c6b
Daniel Malik <mail@fronbasal.de>
2018-05-17 22:12:35 +0200

    feat(parser): implemented deprecated, fallback parser

--- api/api_parser.go
+++ api/api_parser.go
@@ -51,65 +51,97 @@ func Parser(c *gin.Context) {
 	doc.Find("table").Last().Find("tr").Each(func(i int, sel *goquery.Selection) {
 		if i != 0 {
 			var v structs.Substitutes
-			sel.Find("td").Each(func(i int, sel *goquery.Selection) {
-				t := strings.Replace(sel.Text(), "\n", "", -1)
-				t = strings.TrimSpace(t)
-				switch i {
-				// Parse the HTML table into the struct
-				case 0:
-					v.Date = t
-					break
-				case 1:
-					v.Hour = t
-					break
-				case 2:
-					v.Day = t
-					break
-				case 3:
-					v.Teacher = t
-					break
-				case 4:
-					v.Time = t
-					break
-				case 5:
-					v.Subject = t
-					break
-				case 6:
-					v.Type = t
-					break
-				case 7:
-					v.Notes = t
-					break
-				case 8:
-					v.Classes = t
-					break
-				case 9:
-					v.Room = strings.Replace(t, "?", " => ", 1)
-					break
-				case 10:
-					v.After = t
-					break
-				case 11:
-					// Check if there is content
-					v.Cancelled = len(strings.Replace(t, " ", "", -1)) != 0
-					break
-				case 12:
-					matched, err := regexp.MatchString("x|X", t)
-					if err != nil {
-						c.JSON(500, gin.H{"message": "Failed to compile Regex."})
-						return
+			count := len(sel.Find("td").Nodes)
+			if count == 15 {
+				sel.Find("td").Each(func(i int, sel *goquery.Selection) {
+					t := strings.Replace(sel.Text(), "\n", "", -1)
+					t = strings.TrimSpace(t)
+					switch i {
+					// Parse the HTML table into the struct
+					case 0:
+						v.Date = t
+						break
+					case 1:
+						v.Hour = t
+						break
+					case 2:
+						v.Day = t
+						break
+					case 3:
+						v.Teacher = t
+						break
+					case 4:
+						v.Time = t
+						break
+					case 5:
+						v.Subject = t
+						break
+					case 6:
+						v.Type = t
+						break
+					case 7:
+						v.Notes = t
+						break
+					case 8:
+						v.Classes = t
+						break
+					case 9:
+						v.Room = strings.Replace(t, "?", " => ", 1)
+						break
+					case 10:
+						v.After = t
+						break
+					case 11:
+						// Check if there is content
+						v.Cancelled = len(strings.Replace(t, " ", "", -1)) != 0
+						break
+					case 12:
+						matched, err := regexp.MatchString("x|X", t)
+						if err != nil {
+							c.JSON(500, gin.H{"message": "Failed to compile Regex."})
+							return
+						}
+						v.New = matched
+						break
+					case 13:
+						v.Reason = t
+						break
+					case 14:
+						v.Counter = t
+						break
 					}
-					v.New = matched
-					break
-				case 13:
-					v.Reason = t
-					break
-				case 14:
-					v.Counter = t
-					break
-				}
-			})
+				})
+			} else { // Alternative parser, deprecated
+				sel.Find("td font").Each(func(i int, sel *goquery.Selection) {
+					t := strings.Replace(sel.Text(), "\n", "", -1)
+					switch i {
+					case 0:
+						v.Classes = sel.Find("b").Text()
+						break
+					case 1:
+						v.Hour = t
+						break
+					case 2:
+						v.Teacher = strings.Replace(t, "?", " => ", 1)
+						break
+					case 3:
+						v.Subject = t
+						break
+					case 4:
+						v.Room = strings.Replace(t, "?", " => ", 1)
+						break
+					case 5:
+						v.Type = t
+						break
+					case 6:
+						v.Notes += t
+						break
+					}
+				})
+
+			}
 			substitutes = append(substitutes, v)
+
 		}
 	})
 
