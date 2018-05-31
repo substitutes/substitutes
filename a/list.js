efe1738db32cf457d7b670aa7540c9fb2292d651
Daniel Malik <mail@fronbasal.de>
2018-05-31 16:23:43 +0200

    fix: implemented switch for fixing extended/unextended struct configuration

--- a/list.js
+++ a/list.js
@@ -1,17 +1,23 @@
 (() => {
-    $.getJSON("/api/c/{{ .class }}", data => {
+    $.getJSON("/api/c/" + c, data => {
+        if (!data.meta.extended)
+            $('.hide-extended').hide()
         $.each(data.substitutes, (a, b) => {
-            (b.type === "Cancelled") ? $("tbody").append(
-                "<tr class='teal-text text-lighten-2'><td>" +
-                b.hour + "</td><td>" + b.time + "</td><td>" + b.teacher +
-                "</td><td>" + b.subject + "</td><td>" + b.room + "</td><td>" + b.type.replace("Vertretung", "Substitute") +
-                "</td><td>" + b.reason + "</td></tr>") : $("tbody").append(
-                "<tr><td>" + b.hour + "</td><td>" + b.time + "</td><td>" +
-                b.teacher +
-                "</td><td>" + b.subject + "</td><td>" + b.room + "</td><td>" + b.type.replace("Vertretung", "Substitute") +
-                "</td><td>" + b.reason + "</td></tr>");
+            if (data.meta.extended) {
+                $("tbody").append(
+                    "<tr class='text-lighten-2'><td>" +
+                    b.hour + "</td><td>" + b.time + "</td><td>" + b.teacher +
+                    "</td><td>" + b.subject + "</td><td>" + b.room + "</td><td>" + b.type.replace("Vertretung", "Substitute") +
+                    "</td><td>" + b.reason + "</td></tr>");
+            } else {
+                $("tbody").append(
+                    "<tr class='text-lighten-2'><td>" +
+                    b.hour + "</td><td>" + b.teacher +
+                    "</td><td>" + b.subject + "</td><td>" + b.room + "</td><td>" + b.type.replace("Vertretung", "Substitute") +
+                    "</td></tr>");
+            }
         });
         $("h4").html(data.meta.date.replace("Vertretungen", "Substitutes").split("/")[0]);
         $("#title").html(data.meta.class);
     }).catch(m => Materialize.toast(m.status + ": " + m.responseJSON.message));
-});
+})();
